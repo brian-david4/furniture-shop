fd186952de578b0c5ed195c571d17891baa3df04
Brian David <bdavid1530@gmail.com>
2023-10-04 13:38:40 +0100

    background for Home

--- furniture/src/Pages/Home.tsx
+++ furniture/src/Pages/Home.tsx
@@ -1,9 +1,16 @@
 import HomeNavBar from "../Components/HomeNavBar/HomeNavBar";
+import { Img } from "react-image";
+import styles from "./home.module.css";
+import bgWEBP from "/00.webp";
+import bgJPG from "/00.jpg";
 
 const Home = () => {
   return (
     <>
-      <HomeNavBar />
+      <div className={styles.homeBg}>
+        <Img src={[bgWEBP, bgJPG]} />
+        <HomeNavBar />
+      </div>
     </>
   );
 };
