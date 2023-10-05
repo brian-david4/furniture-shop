06f115eb0ae06b610da94ebc55dfedf2dc00ba4e
Brian David <bdavid1530@gmail.com>
2023-10-05 11:28:20 +0100

    Create blur Mouse follow

--- furniture/src/Pages/Home.tsx
+++ furniture/src/Pages/Home.tsx
@@ -1,20 +1,42 @@
 import { Img } from "react-image";
+import { motion } from "framer-motion";
+import { useMousePosition } from "../hooks/useMousePosition";
 import HomeNavBar from "../Components/HomeNavBar/HomeNavBar";
 import HomeFooter from "../Components/HomeFooter/HomeFooter";
 import styles from "./home.module.css";
 import bgWEBP from "/00.webp";
 import bgJPG from "/00.jpg";
+import bgWEBPbw from "/00BW.webp";
+import { useState } from "react";
 
 const Home = () => {
+  const [isHovered, setHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 350 : 75;
+
   return (
     <>
       <div className={styles.homeBg}>
         <Img
-          style={{ userSelect: "none", zIndex: -1 }}
+          className={styles.colorBG}
           draggable={false}
           src={[bgWEBP, bgJPG]}
         />
-        <div className={styles.homeTitle}>
+        <motion.img
+          animate={{
+            WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+            WebkitMaskSize: `${size}px`,
+          }}
+          transition={{ type: "tween", ease: "backOut" }}
+          className={styles.maskBG}
+          src={bgWEBPbw}
+          draggable={false}
+        />
+        <div
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+          className={styles.homeTitle}
+        >
           <h1>perch &</h1>
           <div id={styles.highland}>highland</div>
         </div>
