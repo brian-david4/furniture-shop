27616d7a59cb550cf1208ec134b1acdd3383dbbb
Brian David <bdavid1530@gmail.com>
2023-10-16 14:01:46 +0100

    NavbarTitle links to home

--- furniture/src/Components/NavbarTitle/NavbarTitle.tsx
+++ furniture/src/Components/NavbarTitle/NavbarTitle.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import styles from "./navTitle.module.css";
 
 const NavbarTitle = () => {
   return (
     <>
-      <div className={styles.navTitle}>
-        <h1>perch &</h1>
-        <div id={styles.highland}>highland</div>
-      </div>
+      <Link className={styles.link} to="/">
+        <div className={styles.navTitle}>
+          <h1>perch &</h1>
+          <div id={styles.highland}>highland</div>
+        </div>
+      </Link>
     </>
   );
 };
