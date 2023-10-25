33ea66c8d77ea01ed21e0cf0de8a54b06ce63043
Brian David <bdavid1530@gmail.com>
2023-10-25 15:46:33 +0100

    Cart Menu opens on cart click

--- furniture/src/Components/Cart/Cart.tsx
+++ furniture/src/Components/Cart/Cart.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import styles from "./Cart.module.css";
+import CartMenu from "./CartMenu";
 
 const Cart = () => {
   const [isCartOpen, setCartOpen] = useState(false);
 
   return (
-    <div onClick={() => setCartOpen(!isCartOpen)} className={styles.cartTitle}>
-      cart(0)
-    </div>
+    <>
+      <div
+        onClick={() => setCartOpen(!isCartOpen)}
+        className={styles.cartTitle}
+      >
+        cart(0)
+      </div>
+      {isCartOpen && <CartMenu />}
+    </>
   );
 };
 
---
+++ furniture/src/Components/Cart/CartMenu.tsx
@@ -0,0 +1,11 @@
+import styles from "./Cart.module.css";
+
+const CartMenu = () => {
+  return (
+    <div className={styles.cartMenu}>
+      <h1>cart menu</h1>
+    </div>
+  );
+};
+
+export default CartMenu;
