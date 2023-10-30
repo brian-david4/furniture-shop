566da1656fab21f4dc7c7c6a378623a67cdafe01
Brian David <bdavid1530@gmail.com>
2023-10-30 14:30:38 +0000

    Wrap App with CartProvider

--- furniture/src/App.tsx
+++ furniture/src/App.tsx
@@ -2,17 +2,20 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
 import ProductPage from "./Components/ProductPage/ProductPage";
+import CartProvider from "./Data/CartContext";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Shop />} />
-        <Route path="/products/:productName" element={<ProductPage />} />
-      </Routes>
+      <CartProvider>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Shop />} />
+          <Route path="/products/:productName" element={<ProductPage />} />
+        </Routes>
+      </CartProvider>
     </>
   );
 }
