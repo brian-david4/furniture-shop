2ff82d434dae6d620814f6548252434ecf4678e1
Brian David <bdavid1530@gmail.com>
2023-11-14 15:26:46 +0000

    Items in cart appear in shopping cart modal

--- furniture/src/Components/Cart/CartMenu.tsx
+++ furniture/src/Components/Cart/CartMenu.tsx
@@ -1,19 +1,28 @@
+import { useContext } from "react";
 import CartHeader from "../CartHeader/CartHeader";
 import CartMenuItem from "../CartMenuItem/CartMenuItem";
 import styles from "./Cart.module.css";
 import CartItems from "./CartItems/CartItems";
+import { CartContext } from "../../Data/CartContext";
 
 interface CartMenuProps {
   closeMenu: () => void;
 }
 
 const CartMenu = ({ closeMenu }: CartMenuProps) => {
+  const cart = useContext(CartContext);
+
   return (
     <div className={styles.cartMenu}>
       <CartHeader closeMenu={() => closeMenu()} />
       <CartItems>
-        <CartMenuItem />
-        <CartMenuItem />
+        {cart.items.map((cartItem, idx) => (
+          <CartMenuItem
+            key={idx}
+            id={cartItem.id}
+            quantity={cartItem.quantity}
+          />
+        ))}
       </CartItems>
     </div>
   );
--- furniture/src/Components/CartMenuItem/CartMenuItem.tsx
+++ furniture/src/Components/CartMenuItem/CartMenuItem.tsx
@@ -1,9 +1,21 @@
 import styles from "./CartmenuItem.module.css";
+import { products } from "../../Data/data";
 
-const CartMenuItem = () => {
+interface CartMenuItemProps {
+  id: number;
+  quantity: number;
+}
+
+const CartMenuItem = ({ id, quantity }: CartMenuItemProps) => {
+  const product = products.find((prod) => prod.id.id === id);
+
   return (
     <>
-      <div className={styles.itemContainer}></div>
+      <div className={styles.itemContainer}>
+        {product === undefined && <></>}
+        <h2>{product?.id.name}</h2>
+        <h3>{quantity} in cart</h3>
+      </div>
     </>
   );
 };
