ce52924353c46e234732cca7e52a5fff35d66326
Brian David <bdavid1530@gmail.com>
2023-11-17 18:08:04 +0000

    give CartItem image widht: 140px

--- furniture/src/Data/data.ts
+++ furniture/src/Data/data.ts
@@ -8,7 +8,7 @@ import side_table from "/product-images/side_table.webp";
 
 export const products: Product[] = [
   {
-    id: { name: "ALUMINIUM OTTOMAN", slug: "aluminium-ottoman", id: 1 },
+    id: { name: "OTTOMAN", slug: "aluminium-ottoman", id: 1 },
     description:
       "La Pepino is a collection of seating variations, built on the repeating pattern of cylindrical cushions, and supported by an organically shaped base. Every piece comes fully upholstered in customisable fabrics, and plays with the different points of contact between cushion and body",
     price: 1200,
