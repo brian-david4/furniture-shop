95ecd88696ad9488581e8ec157709447b3df9b17
Brian David <bdavid1530@gmail.com>
2023-11-22 15:17:12 +0000

    Create checkout page

--- furniture/src/App.tsx
+++ furniture/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
 import ProductPage from "./Components/ProductPage/ProductPage";
 import CartProvider from "./Data/CartContext";
+import CheckoutPage from "./Pages/CheckoutPage/CheckoutPage";
 
 function App() {
   const location = useLocation();
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Shop />} />
           <Route path="/products/:productName" element={<ProductPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
         </Routes>
       </CartProvider>
     </>
---
+++ furniture/src/Pages/CheckoutPage/CheckoutPage.tsx
@@ -0,0 +1,11 @@
+import styles from "./Checkout.module.css";
+
+const CheckoutPage = () => {
+  return (
+    <>
+      <div className={styles.checkoutContainer}></div>
+    </>
+  );
+};
+
+export default CheckoutPage;
