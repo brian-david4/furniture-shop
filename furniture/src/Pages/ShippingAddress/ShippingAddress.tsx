95e3fca0926fb04391009dc8338f816f0d9aeac1
Brian David <bdavid1530@gmail.com>
2023-12-04 17:07:29 +0000

    Create Delivery Form for /shipping-address

--- furniture/src/Pages/ShippingAddress/ShippingAddress.tsx
+++ furniture/src/Pages/ShippingAddress/ShippingAddress.tsx
@@ -1,7 +1,65 @@
+import CheckoutNavbar from "../../Components/CheckoutNavbar/CheckoutNavbar";
+import SummaryBox from "../../Components/SummaryBox/SummaryBox";
 import styles from "./shipaddress.module.css";
 
 const ShippingAddress = () => {
-  return <div className={styles.shipaddress}>ShippingAddress</div>;
+  return (
+    <>
+      <CheckoutNavbar />
+      <div className={styles.bodyPlacement}>
+        <div className={styles.addressForm}>
+          <h1 className={styles.deliveryTitle}>Delivery</h1>
+          <div className={styles.nameField}>
+            <div className={styles.firstNameField}>
+              <input
+                className={styles.inputText}
+                type="text"
+                name="first-name"
+                placeholder="First Name*"
+              />
+            </div>
+            <input
+              className={styles.inputText}
+              type="text"
+              placeholder="Last Name*"
+            />
+          </div>
+          <div className={styles.deliveryAddress}>
+            <input
+              className={styles.inputText}
+              type="text"
+              placeholder="Address Line 1*"
+            />
+            <div className={styles.townPostcode}>
+              <input
+                className={styles.inputText}
+                type="text"
+                placeholder="Town/City*"
+              />{" "}
+              <input
+                className={styles.inputText}
+                type="text"
+                placeholder="Postcode*"
+              />
+            </div>
+            <div className={styles.emailPhone}>
+              <input
+                className={styles.inputText}
+                type="text"
+                placeholder="Email*"
+              />{" "}
+              <input
+                className={styles.inputText}
+                type="text"
+                placeholder="Phone Number*"
+              />
+            </div>
+          </div>
+        </div>
+        <SummaryBox disabled={false} />
+      </div>
+    </>
+  );
 };
 
 export default ShippingAddress;
